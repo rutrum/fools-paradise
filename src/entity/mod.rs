7bcd780257272222dbf14b67ca0f880afa40b98a
David Purdum <purdum41@gmail.com>
2022-01-19 15:10:57 -0500

    Rewrite/refactor game state and logic

--- src/entity/mod.rs
+++ src/entity/mod.rs
@@ -18,13 +18,6 @@ pub use blaster::Blaster;
 mod turret;
 pub use turret::Turret;
 
-/*
-pub enum EnemyType {
-    Simple(Enemy),
-    Turret(Turret),
-}
-*/
-
 pub trait Movement : Render {
     /// Mutable reference to the true x position.
     fn x_pos_mut(&mut self) -> &mut f32;
